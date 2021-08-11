a04d2a9c775af1135142cd30571ac1e0313e035c
Pierre Kisters <pierrekisters@gmail.com>
2021-08-11 14:45:21 +0200

    removed Response.liftResource. Use Client.toHttpApp instead

--- src/main/scala/de/lolhens/http4s/proxy/Http4sProxy.scala
+++ src/main/scala/de/lolhens/http4s/proxy/Http4sProxy.scala
@@ -1,12 +1,7 @@
 package de.lolhens.http4s.proxy
 
-import cats.Monad
-import cats.effect.{BracketThrow, Resource}
-import cats.syntax.apply._
-import cats.syntax.functor._
-import fs2.Stream
 import org.http4s.headers.Host
-import org.http4s.{Request, Response, Uri}
+import org.http4s.{Request, Uri}
 
 object Http4sProxy {
   implicit class RequestOps[F[_]](val request: Request[F]) extends AnyVal {
@@ -14,17 +9,6 @@ object Http4sProxy {
       request
         .withUri(destination)
         .putHeaders(Host(destination.authority.map(_.host.value).getOrElse(""), destination.port))
-  }
-
-  implicit class ResponseCompanionOps(val self: Response.type) extends AnyVal {
-    def liftResource[F[_] : BracketThrow](resource: Resource[F, Response[F]]): F[Response[F]] =
-      resource.allocated.map {
-        case (response, release) =>
-          response.withBodyStream(
-            Stream.resource(Resource.make(Monad[F].unit)(_ => release)) *>
-              response.body
-          )
-      }
   }
 
   implicit class UriOps(val uri: Uri) extends AnyVal {
